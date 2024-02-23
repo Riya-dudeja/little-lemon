30210baeca611deae21b5d0a84a3d8188801b716
Riya <riyadudeja264@gmail.com>
2024-02-23 15:35:54 +0530

    changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
-import main from "./icons_assets/main.jpg"
+import main from "./icons_assets/main.jpg";
+import {NavLink} from "react-router-dom";
 
 function Main(){
   return (
@@ -8,7 +9,9 @@ function Main(){
         <h1 className="title">Little Lemon</h1>
         <h3>Chicago</h3>
         <p className="main-info">We are a charming neighborhood bistro that serves good food and classic drinks in a lively but casual environment. The restaurant features a locally sourced menu with daily specials.</p>
-        <button>Reserve a table</button>
+        <NavLink to="booking">
+          <button aria-label="On Click">Reserve a table</button>
+        </NavLink>
       </article>
       <aside>
         <img src= {main} className="main-img" alt="Chef holding a serving of our famous sliders"/>
